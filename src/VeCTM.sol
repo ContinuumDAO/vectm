65a9f5f80c0965e4dd55ff814d46a72964e43373
Hal <73549429+hal0177@users.noreply.github.com>
2023-09-26 21:00:48 +0100

    base configuration added for veCTM

--- src/VeCTM.sol
+++ src/VeCTM.sol
@@ -1,5 +1,38 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 pragma solidity ^0.8.21;
 
+import "openzeppelin/token/ERC721/ERC721.sol";
+import "openzeppelin/utils/cryptography/EIP712.sol";
+import "openzeppelin/token/ERC721/extensions/ERC721Enumerable.sol";
+import "openzeppelin/token/ERC721/extensions/ERC721URIStorage.sol";
+import "openzeppelin/governance/utils/Votes.sol";
 
-contract VeCTM {}
+
+contract VeCTM is ERC721, ERC721Enumerable, ERC721URIStorage, Votes {
+    constructor() ERC721("My Token", "MYT") EIP712("My Token", "1") {}
+
+    function _beforeTokenTransfer(address from, address to, uint256 firstTokenId, uint256 batchSize) internal override(ERC721, ERC721Enumerable) {
+        super._beforeTokenTransfer(from, to , firstTokenId, batchSize);
+    }
+
+    function _afterTokenTransfer(address from, address to, uint256 firstTokenId, uint256 batchSize) internal override(ERC721) {
+        _transferVotingUnits(from, to, batchSize);
+        super._afterTokenTransfer(from, to, firstTokenId, batchSize);
+    }
+
+    function _getVotingUnits(address account) internal view override returns (uint256) {
+        return balanceOf(account);
+    }
+
+    function _burn(uint256 _tokenId) internal override(ERC721, ERC721URIStorage) {
+        super._burn(_tokenId);
+    }
+
+    function tokenURI(uint256 _tokenId) public view override(ERC721, ERC721URIStorage) returns (string memory uri) {
+        return super.tokenURI(_tokenId);
+    }
+
+    function supportsInterface(bytes4 interfaceId) public view override(ERC721, ERC721Enumerable, ERC721URIStorage) returns (bool) {
+        return super.supportsInterface(interfaceId);
+    }
+}
