587a2a1f958f87a3499a1d39153e403f0f909a5e
Hal <73549429+hal0177@users.noreply.github.com>
2023-11-12 15:22:35 +0000

    selecting modules for use in voting escrow

--- src/CTM.sol
+++ src/CTM.sol
@@ -1,9 +1,14 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 pragma solidity ^0.8.21;
 
-// import "openzeppelin/token/ERC20/ERC20.sol";
+import "openzeppelin/token/ERC20/ERC20.sol";
 
 
-// contract CTM is ERC20 {
-//     constructor() ERC20("Continuum", "CTM") {}
-// }
+contract CTM is ERC20 {
+    constructor() ERC20("Continuum", "CTM") {}
+
+    // TEST ONLY
+    function print(address recipient, uint256 amount) public {
+        _mint(recipient, amount);
+    }
+}
--- test/VeCTMTest.t.sol
+++ test/VeCTMTest.t.sol
@@ -2,13 +2,24 @@
 pragma solidity ^0.8.21;
 
 import "forge-std/Test.sol";
-import {VeCTM} from "../src/VeCTM.sol";
+import {VotingEscrowContinuum} from "../src/VotingEscrowContinuum.sol";
+import {CTM} from "../src/CTM.sol";
 
 
 contract VeCTMTest is Test {
-    VeCTM veCTM;
+    CTM ctm;
+    VotingEscrowContinuum vectm;
 
     function setUp() public {
-        veCTM = new VeCTM();
+        ctm = new CTM();
+        vectm = new VotingEscrowContinuum(address(ctm), "https://example.com/");
+        ctm.print(address(this), 10 ether);
+        ctm.approve(address(vectm), 10 ether);
+    }
+
+    function test_getURI() public {
+        uint tokenId = vectm.create_lock(1 ether, block.timestamp + 2 weeks);
+        string memory uri = vectm.tokenURI(tokenId);
+        console.log(uri);
     }
 }
