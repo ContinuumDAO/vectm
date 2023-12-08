edd39c328f1b9552426dc90a36f36cbaf50c71eb
Hal <73549429+hal0177@users.noreply.github.com>
2023-12-08 10:05:47 +0000

    cleaned up workspace compiler/remappings errors

--- src/VotingEscrowProxy.sol
+++ src/VotingEscrowProxy.sol
@@ -1,13 +1,17 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 pragma solidity ^0.8.23;
 
-import {TransparentUpgradeableProxy} from "openzeppelin-contracts/contracts/proxy/transparent/TransparentUpgradeableProxy.sol";
+import {TransparentUpgradeableProxy} from "@openzeppelin/contracts/proxy/transparent/TransparentUpgradeableProxy.sol";
 
 
 contract VotingEscrowProxy is TransparentUpgradeableProxy {
     constructor(address _logic, address initialOwner) TransparentUpgradeableProxy(_logic, initialOwner, "") {}
 
-    function admin() public view returns (address) {
+    function admin() public returns (address) {
         return _proxyAdmin();
+    }
+
+    receive() payable external {
+        _fallback();
     }
 }
